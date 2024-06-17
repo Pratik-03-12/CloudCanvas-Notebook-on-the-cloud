e0ed6fccb039c24676dfab7bf14b42dbd4b07817
Pratik-03-12 <sahapratik390@gmail.com>
2024-06-18 02:31:27 +0530

    "third commit"

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,30 @@
-const express=require('express');
-const User = require('../models/User');
-const router=express.Router();
-
+const express = require("express");
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
+const router = express.Router();
 
 //Create a user using POST "/api/auth"
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user=User(req.body);
-    user.save();
-    res.send(req.body)
-}
-)
-module.exports=router;
+router.post(
+  "/",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 7,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email:req.body.email
+    }).then((user) => res.json(user)).catch(err=>{console.log(err)
+        res.json({error:"Please Enter a unique value"})
+    });
+  }
+);
+module.exports = router;
